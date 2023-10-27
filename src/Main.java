e46c78322df9100e61db8a6149f31c35174d7622
MarcoFerrel <108032978+MarcoFerrel@users.noreply.github.com>
2023-10-26 21:29:44 -0600

    Hello

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,18 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-//penis
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        long startTime = System.nanoTime();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        // Run your algorithm or code here
+        for (int i = 0; i < 10; i++) {
+            System.out.println("helllo");
         }
+        // Replace this with the actual algorithm you want to time
+
+        long endTime = System.nanoTime();
+        long executionTime = endTime - startTime;
+
+        // Convert nanoseconds to milliseconds for readability
+        double executionTimeMilliseconds = (double) executionTime / 1_000_000.0;
     }
 }
