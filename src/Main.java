b97412891709a6dde1d8a6c6261788ae5e74d232
MarcoFerrel <108032978+MarcoFerrel@users.noreply.github.com>
2023-10-28 18:10:49 -0600

    Insertion

--- src/Main.java
+++ src/Main.java
@@ -47,6 +47,29 @@ public class Main {
             }
         }
     }
+    public static void selectionSort(int [] numbers) {
+        int i;
+        int j;
+        int indexSmallest;
+        int temp;      // Temporary variable for swap
+
+        for (i = 0; i < numbers.length - 1; ++i) {
+
+            // Find index of smallest remaining element
+            indexSmallest = i;
+            for (j = i + 1; j < numbers.length; ++j) {
+
+                if (numbers[j] < numbers[indexSmallest]) {
+                    indexSmallest = j;
+                }
+            }
+
+            // Swap numbers[i] and numbers[indexSmallest]
+            temp = numbers[i];
+            numbers[i] = numbers[indexSmallest];
+            numbers[indexSmallest] = temp;
+        }
+    }
 
 
 }
