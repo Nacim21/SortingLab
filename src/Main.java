823da7dd636769a04c081902ce84519a614125ed
MarcoFerrel <108032978+MarcoFerrel@users.noreply.github.com>
2023-10-28 18:15:57 -0600

    Insertion

--- src/Main.java
+++ src/Main.java
@@ -70,6 +70,26 @@ public class Main {
             numbers[indexSmallest] = temp;
         }
     }
+    public static void insertionSort(int [] numbers) {
+        int i;
+        int j;
+        int temp;      // Temporary variable for swap
+
+        for (i = 1; i < numbers.length; ++i) {
+            j = i;
+            // Insert numbers[i] into sorted part
+            // stopping once numbers[i] in correct position
+            while (j > 0 && numbers[j] < numbers[j - 1]) {
+
+                // Swap numbers[j] and numbers[j - 1]
+                temp = numbers[j];
+                numbers[j] = numbers[j - 1];
+                numbers[j - 1] = temp;
+                --j;
+            }
+        }
+    }
+
 
 
 }
